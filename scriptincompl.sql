83ab2dc681215e5a6da7bda2016db697e44e947e
nenalf <106717376+nenalf@users.noreply.github.com>
2024-05-23 11:48:01 -0300

    Update scriptincompl.sql

--- scriptincompl.sql
+++ scriptincompl.sql
@@ -5,20 +5,21 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Energia
+-- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
 
 -- -----------------------------------------------------
--- Schema Energia
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Energia` DEFAULT CHARACTER SET utf8 ;
-USE `Energia` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `Energia`.`Localidade`
+-- Table `mydb`.`Localidade`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Energia`.`Localidade` (
-  `idLocalidade` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`Localidade` (
+  `idLocalidade` INT NOT NULL AUTO_INCREMENT,
   `Região` VARCHAR(45) NULL,
   `Estado` VARCHAR(45) NULL,
   `Localidadecol` VARCHAR(45) NULL,
@@ -27,10 +28,10 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Energia`.`Tempo`
+-- Table `mydb`.`Tempo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Energia`.`Tempo` (
-  `idTempo` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`Tempo` (
+  `idTempo` INT NOT NULL AUTO_INCREMENT,
   `Mes` INT NULL,
   `Ano` INT NULL,
   PRIMARY KEY (`idTempo`))
@@ -38,20 +39,19 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Energia`.`Fonte de Energia`
+-- Table `mydb`.`Fonte de Energia`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Energia`.`Fonte de Energia` (
-  `idTipo de Energia` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`Fonte de Energia` (
+  `idTipo de Energia` INT NOT NULL AUTO_INCREMENT,
   `Descrilção do Tipo` VARCHAR(45) NULL,
   PRIMARY KEY (`idTipo de Energia`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Energia`.`Fato - Energia`
+-- Table `mydb`.`Fato - Energia`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Energia`.`Fato - Energia` (
-  `idFato - Energia` INT ZEROFILL NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`Fato - Energia` (
   `Qtde Coletada` DECIMAL(12,3) NULL,
   `Qtde Programada` DECIMAL(12,3) NULL,
   `Fator de Geração` DECIMAL(6,2) NULL,
@@ -60,23 +60,23 @@ CREATE TABLE IF NOT EXISTS `Energia`.`Fato - Energia` (
   `Fonte de Energia_idTipo de Energia` INT NOT NULL,
   `Qtde Verificada` DECIMAL(12,3) NULL,
   `Fato - Energiacol` VARCHAR(45) NULL,
-  PRIMARY KEY (`idFato - Energia`, `Tempo_idTempo`, `Localidade_idLocalidade`, `Fonte de Energia_idTipo de Energia`),
+  PRIMARY KEY (`Tempo_idTempo`, `Localidade_idLocalidade`, `Fonte de Energia_idTipo de Energia`),
   INDEX `fk_Fato - Energia_Tempo_idx` (`Tempo_idTempo` ASC) VISIBLE,
   INDEX `fk_Fato - Energia_Localidade1_idx` (`Localidade_idLocalidade` ASC) VISIBLE,
   INDEX `fk_Fato - Energia_Fonte de Energia1_idx` (`Fonte de Energia_idTipo de Energia` ASC) VISIBLE,
   CONSTRAINT `fk_Fato - Energia_Tempo`
     FOREIGN KEY (`Tempo_idTempo`)
-    REFERENCES `Energia`.`Tempo` (`idTempo`)
+    REFERENCES `mydb`.`Tempo` (`idTempo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Fato - Energia_Localidade1`
     FOREIGN KEY (`Localidade_idLocalidade`)
-    REFERENCES `Energia`.`Localidade` (`idLocalidade`)
+    REFERENCES `mydb`.`Localidade` (`idLocalidade`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Fato - Energia_Fonte de Energia1`
     FOREIGN KEY (`Fonte de Energia_idTipo de Energia`)
-    REFERENCES `Energia`.`Fonte de Energia` (`idTipo de Energia`)
+    REFERENCES `mydb`.`Fonte de Energia` (`idTipo de Energia`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
