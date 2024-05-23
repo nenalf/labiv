0bc11d418921f3032baed2ee144e63d3bf9f2535
nenalf <106717376+nenalf@users.noreply.github.com>
2024-05-23 11:42:36 -0300

    Update scriptincompl.sql

--- scriptincompl.sql
+++ scriptincompl.sql
@@ -1,46 +1,87 @@
-DROP TABLE IF EXISTS `matricula` ;
-CREATE TABLE `fatorCapacidade` (
-	`id_subsistema` int DEFAULT NULL,
-	`nom_subsistema` int DEFAULT NULL,
-	`id_estado` text DEFAULT NULL,
-	`nom_estado` text DEFAULT NULL, 
-	`nom_pontoconexao` text DEFAULT NULL, 
-	`nom_localizacao` text DEFAULT NULL,
-    `val_latitudesecoletora` decimal DEFAULT NULL,
-    `val_longitudesecoletora` decimal DEFAULT NULL,	
-    `val_latitudepontoconexao` decimal DEFAULT NULL,
-    `val_longitudepontoconexao` decimal DEFAULT NULL,
-    `nom_modalidadeoperacao` text DEFAULT NULL, 	
-    `nom_tipousina`	text DEFAULT NULL,
-    `nom_usina_conjunto` text DEFAULT NULL, 	
-    `id_ons` text DEFAULT NULL, 
-	`ceg` text DEFAULT NULL, 	
-    `din_instante` datetime default null,
-    `val_geracaoprogramada`	decimal DEFAULT NULL,
-    `val_geracaoverificada`	decimal DEFAULT NULL,
-    `val_capacidadeinstalada` decimal DEFAULT NULL,
-	`val_fatorcapacidade` decimal DEFAULT NULL
-    );
-    
-    select * from fator_capacidade_2021;
-    
-    ALTER TABLE fator_capacidade_2021 DROP COLUMN `nom_subsistema`;
-    
-	ALTER TABLE fator_capacidade_2021 DROP COLUMN  `nom_estado`;
-    ALTER TABLE fator_capacidade_2021 DROP COLUMN  `nom_pontoconexao`; 
-    ALTER TABLE fator_capacidade_2021 DROP COLUMN  `val_latitudesecoletora`;
-    ALTER TABLE fator_capacidade_2021 DROP COLUMN  `val_longitudesecoletora`;
-    ALTER TABLE fator_capacidade_2021 DROP COLUMN  `val_latitudepontoconexao`;
-ALTER TABLE fator_capacidade_2021 DROP COLUMN  `val_longitudepontoconexao`;
+-- MySQL Workbench Forward Engineering
 
-ALTER TABLE fator_capacidade_2021 DROP COLUMN  `nom_usina_conjunto`;
-ALTER TABLE fator_capacidade_2021 DROP COLUMN  `nom_modalidadeoperacao`;
-ALTER TABLE fator_capacidade_2021 DROP COLUMN  `id_ons`;
-ALTER TABLE fator_capacidade_2021 DROP COLUMN  `CEG`;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-   
-SELECT * FROM fator_capacidade_2021;
+-- -----------------------------------------------------
+-- Schema Energia
+-- -----------------------------------------------------
 
-SELECT COUNT(*) FROM segundatabela;
+-- -----------------------------------------------------
+-- Schema Energia
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Energia` DEFAULT CHARACTER SET utf8 ;
+USE `Energia` ;
+
+-- -----------------------------------------------------
+-- Table `Energia`.`Localidade`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Energia`.`Localidade` (
+  `idLocalidade` INT NOT NULL,
+  `Região` VARCHAR(45) NULL,
+  `Estado` VARCHAR(45) NULL,
+  `Localidadecol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idLocalidade`))
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `Energia`.`Tempo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Energia`.`Tempo` (
+  `idTempo` INT NOT NULL,
+  `Mes` INT NULL,
+  `Ano` INT NULL,
+  PRIMARY KEY (`idTempo`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Energia`.`Fonte de Energia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Energia`.`Fonte de Energia` (
+  `idTipo de Energia` INT NOT NULL,
+  `Descrilção do Tipo` VARCHAR(45) NULL,
+  PRIMARY KEY (`idTipo de Energia`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Energia`.`Fato - Energia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Energia`.`Fato - Energia` (
+  `idFato - Energia` INT ZEROFILL NOT NULL,
+  `Qtde Coletada` DECIMAL(12,3) NULL,
+  `Qtde Programada` DECIMAL(12,3) NULL,
+  `Fator de Geração` DECIMAL(6,2) NULL,
+  `Tempo_idTempo` INT NOT NULL,
+  `Localidade_idLocalidade` INT NOT NULL,
+  `Fonte de Energia_idTipo de Energia` INT NOT NULL,
+  `Qtde Verificada` DECIMAL(12,3) NULL,
+  `Fato - Energiacol` VARCHAR(45) NULL,
+  PRIMARY KEY (`idFato - Energia`, `Tempo_idTempo`, `Localidade_idLocalidade`, `Fonte de Energia_idTipo de Energia`),
+  INDEX `fk_Fato - Energia_Tempo_idx` (`Tempo_idTempo` ASC) VISIBLE,
+  INDEX `fk_Fato - Energia_Localidade1_idx` (`Localidade_idLocalidade` ASC) VISIBLE,
+  INDEX `fk_Fato - Energia_Fonte de Energia1_idx` (`Fonte de Energia_idTipo de Energia` ASC) VISIBLE,
+  CONSTRAINT `fk_Fato - Energia_Tempo`
+    FOREIGN KEY (`Tempo_idTempo`)
+    REFERENCES `Energia`.`Tempo` (`idTempo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Fato - Energia_Localidade1`
+    FOREIGN KEY (`Localidade_idLocalidade`)
+    REFERENCES `Energia`.`Localidade` (`idLocalidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Fato - Energia_Fonte de Energia1`
+    FOREIGN KEY (`Fonte de Energia_idTipo de Energia`)
+    REFERENCES `Energia`.`Fonte de Energia` (`idTipo de Energia`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
